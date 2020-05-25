f58b5d2e36fd61c8af0e5fd148d5883c542a015d
Bogdan <bogdans.afonins@thethingsindustries.com>
2020-05-25 16:49:09 +0300

    console: Add device wizard prompt component

---
+++ pkg/webui/console/views/device-add/wizard/prompt/index.js
@@ -0,0 +1,81 @@
+// Copyright © 2020 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+import { defineMessages } from 'react-intl'
+
+import Prompt from '@ttn-lw/components/prompt'
+
+import Message from '@ttn-lw/lib/components/message'
+
+import sharedMessages from '@ttn-lw/lib/shared-messages'
+import PropTypes from '@ttn-lw/lib/prop-types'
+import useBeforeUnload from '@ttn-lw/lib/hooks/use-before-unload'
+
+const m = defineMessages({
+  modalTitle: 'Abort end device creation',
+  modalMessage:
+    'Are you sure you want to abort creation of this end device? Unsaved changes in your forms will be lost.',
+  modalApproveText: 'Dismiss and proceed',
+})
+
+const WizardPromptModal = React.memo(props => {
+  const { when, onApprove, onCancel, shouldBlockNavigation } = props
+
+  useBeforeUnload(event => {
+    if (when) {
+      event.preventDefault()
+    }
+  })
+
+  const handleShouldBlockNavigation = React.useCallback(
+    location => {
+      return shouldBlockNavigation(location)
+    },
+    [shouldBlockNavigation],
+  )
+
+  const modalProps = React.useMemo(() => {
+    return {
+      title: m.modalTitle,
+      buttonMessage: m.modalApproveText,
+      cancelButtonMessage: sharedMessages.cancel,
+    }
+  }, [])
+
+  return (
+    <Prompt
+      modal={modalProps}
+      when={when}
+      onApprove={onApprove}
+      onCancel={onCancel}
+      shouldBlockNavigation={handleShouldBlockNavigation}
+    >
+      <Message content={m.modalMessage} />
+    </Prompt>
+  )
+})
+
+WizardPromptModal.propTypes = {
+  onApprove: PropTypes.func,
+  onCancel: PropTypes.func,
+  shouldBlockNavigation: PropTypes.func.isRequired,
+  when: PropTypes.bool.isRequired,
+}
+
+WizardPromptModal.defaultProps = {
+  onApprove: () => null,
+  onCancel: () => null,
+}
+export default WizardPromptModal
