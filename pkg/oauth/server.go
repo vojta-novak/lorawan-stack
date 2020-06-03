nejraselimovic/lorawan-stack-1

oauth,util: Enable authorization via session cookie

pkg/oauth/server.go
@@ -21,13 +21,13 @@ import (
 	"time"
 
 	echo "github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"github.com/openshift/osin"
 	"go.thethings.network/lorawan-stack/v3/pkg/errors"
 	web_errors "go.thethings.network/lorawan-stack/v3/pkg/errors/web"
 	"go.thethings.network/lorawan-stack/v3/pkg/identityserver/store"
 	"go.thethings.network/lorawan-stack/v3/pkg/log"
 	"go.thethings.network/lorawan-stack/v3/pkg/web"
+	"go.thethings.network/lorawan-stack/v3/pkg/web/middleware"
 	"go.thethings.network/lorawan-stack/v3/pkg/webui"
 )
 
@@ -199,7 +199,7 @@ func (s *server) output(c echo.Context, resp *osin.Response) error {
 }
 
 func (s *server) RegisterRoutes(server *web.Server) {
-	group := server.Group(
+	root := server.Group(
 		s.config.Mount,
 		func(next echo.HandlerFunc) echo.HandlerFunc {
 			return func(c echo.Context) error {
@@ -220,28 +220,23 @@ func (s *server) RegisterRoutes(server *web.Server) {
 		}),
 	)
 
-	api := group.Group("/api", middleware.CSRF())
+	csrfMiddleware := middleware.CSRF("_csrf", s.config.Mount, s.config.CSRFAuthKey)
+
+	api := root.Group("/api", csrfMiddleware)
 	api.POST("/auth/login", s.Login)
 	api.POST("/auth/logout", s.Logout, s.requireLogin)
 	api.GET("/me", s.CurrentUser, s.requireLogin)
 
-	page := group.Group("", middleware.CSRFWithConfig(middleware.CSRFConfig{
-		TokenLookup: "form:csrf",
-	}))
+	page := root.Group("", csrfMiddleware)
 	page.GET("/login", webui.Template.Handler, s.redirectToNext)
 	page.GET("/logout", s.ClientLogout)
 	page.GET("/authorize", s.Authorize(webui.Template.Handler), s.redirectToLogin)
 	page.POST("/authorize", s.Authorize(webui.Template.Handler), s.redirectToLogin)
 
-	if s.config.Mount != "" && s.config.Mount != "/" {
-		group.GET("", webui.Template.Handler, s.redirectToLogin)
-	} else {
-		server.GET(s.config.Mount, webui.Template.Handler, s.redirectToLogin)
-	}
-	group.GET("/*", webui.Template.Handler, s.redirectToLogin)
+	page.GET("/", webui.Template.Handler, s.redirectToLogin)
+	page.GET("/*", webui.Template.Handler)
+	page.GET("/local-callback", s.redirectToLocal)
 
 	// No CSRF here:
-	group.GET("/code", webui.Template.Handler)
-	group.GET("/local-callback", s.redirectToLocal)
-	group.POST("/token", s.Token)
+	root.POST("/token", s.Token)
 }

pkg/oauth/user.go
@@ -39,19 +39,14 @@ const authCookieName = "_session"
 func (s *server) authCookie() *cookie.Cookie {
 	return &cookie.Cookie{
 		Name:     authCookieName,
-		Path:     s.config.UI.MountPath(),
+		Path:     "/",
 		HTTPOnly: true,
 	}
 }
 
-type authCookie struct {
-	UserID    string `json:"user_id"`
-	SessionID string `json:"session_id"`
-}
-
 var errAuthCookie = errors.DefineUnauthenticated("auth_cookie", "could not get auth cookie")
 
-func (s *server) getAuthCookie(c echo.Context) (cookie authCookie, err error) {
+func (s *server) getAuthCookie(c echo.Context) (cookie auth.CookieShape, err error) {
 	ok, err := s.authCookie().Get(c, &cookie)
 	if err != nil {
 		return cookie, err
@@ -62,8 +57,8 @@ func (s *server) getAuthCookie(c echo.Context) (cookie authCookie, err error) {
 	return cookie, nil
 }
 
-func (s *server) updateAuthCookie(c echo.Context, update func(value *authCookie) error) error {
-	cookie := &authCookie{}
+func (s *server) updateAuthCookie(c echo.Context, update func(value *auth.CookieShape) error) error {
+	cookie := &auth.CookieShape{}
 	_, err := s.authCookie().Get(c, cookie)
 	if err != nil {
 		return err
@@ -119,7 +114,7 @@ func (s *server) getUser(c echo.Context) (*ttnpb.User, error) {
 	}
 	user, err := s.store.GetUser(
 		c.Request().Context(),
-		&ttnpb.UserIdentifiers{UserID: session.UserID},
+		&ttnpb.UserIdentifiers{UserID: session.UserIDs.UserID},
 		nil,
 	)
 	if err != nil {
@@ -192,16 +187,32 @@ func (s *server) Login(c echo.Context) error {
 		return err
 	}
 	userIDs := ttnpb.UserIdentifiers{UserID: req.UserID}
+	tokenID, err := auth.GenerateID(ctx)
+	if err != nil {
+		return err
+	}
+	tokenKey, err := auth.GenerateKey(ctx)
+	if err != nil {
+		return err
+	}
+	hashedKey, err := auth.Hash(ctx, tokenKey)
+	if err != nil {
+		return err
+	}
 	session, err := s.store.CreateSession(ctx, &ttnpb.UserSession{
-		UserIdentifiers: userIDs,
+		UserIDs:  userIDs,
+		TokenID:  tokenID,
+		TokenKey: hashedKey,
 	})
 	if err != nil {
 		return err
 	}
 	events.Publish(evtUserLogin(ctx, userIDs, nil))
-	err = s.updateAuthCookie(c, func(cookie *authCookie) error {
-		cookie.UserID = session.UserID
+	err = s.updateAuthCookie(c, func(cookie *auth.CookieShape) error {
+		cookie.UserID = session.UserIDs.UserID
 		cookie.SessionID = session.SessionID
+		cookie.TokenKey = tokenKey
+		cookie.TokenID = session.TokenID
 		return nil
 	})
 	if err != nil {
@@ -266,8 +277,8 @@ func (s *server) ClientLogout(c echo.Context) error {
 		return err
 	}
 	if session != nil {
-		events.Publish(evtUserLogout(ctx, session.UserIdentifiers, nil))
-		if err = s.store.DeleteSession(ctx, &session.UserIdentifiers, session.SessionID); err != nil {
+		events.Publish(evtUserLogout(ctx, session.UserIDs, nil))
+		if err = s.store.DeleteSession(ctx, &session.UserIDs, session.SessionID); err != nil {
 			return err
 		}
 	}
@@ -285,8 +296,8 @@ func (s *server) Logout(c echo.Context) error {
 	if err != nil {
 		return err
 	}
-	events.Publish(evtUserLogout(ctx, session.UserIdentifiers, nil))
-	if err = s.store.DeleteSession(ctx, &session.UserIdentifiers, session.SessionID); err != nil {
+	events.Publish(evtUserLogout(ctx, session.UserIDs, nil))
+	if err = s.store.DeleteSession(ctx, &session.UserIDs, session.SessionID); err != nil {
 		return err
 	}
 	s.removeAuthCookie(c)

vojta-novak/lorawan-stack

pkg/oauth/server.go
@@ -21,13 +21,13 @@ import (
 	"time"
 
 	echo "github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"github.com/openshift/osin"
 	"go.thethings.network/lorawan-stack/v3/pkg/errors"
 	web_errors "go.thethings.network/lorawan-stack/v3/pkg/errors/web"
 	"go.thethings.network/lorawan-stack/v3/pkg/identityserver/store"
 	"go.thethings.network/lorawan-stack/v3/pkg/log"
 	"go.thethings.network/lorawan-stack/v3/pkg/web"
+	"go.thethings.network/lorawan-stack/v3/pkg/web/middleware"
 	"go.thethings.network/lorawan-stack/v3/pkg/webui"
 )
 
@@ -199,7 +199,7 @@ func (s *server) output(c echo.Context, resp *osin.Response) error {
 }
 
 func (s *server) RegisterRoutes(server *web.Server) {
-	group := server.Group(
+	root := server.Group(
 		s.config.Mount,
 		func(next echo.HandlerFunc) echo.HandlerFunc {
 			return func(c echo.Context) error {
@@ -220,29 +220,23 @@ func (s *server) RegisterRoutes(server *web.Server) {
 		}),
 	)
 
-	api := group.Group("/api", middleware.CSRFWithConfig(middleware.CSRFConfig{
-		CookieName: "_oauth_csrf",
-		CookiePath: s.config.Mount,
-	}))
+	csrfMiddleware := middleware.CSRF("_csrf", s.config.Mount, s.config.CSRFAuthKey)
+
+	api := root.Group("/api", csrfMiddleware)
 	api.POST("/auth/login", s.Login)
 	api.POST("/auth/logout", s.Logout, s.requireLogin)
 	api.GET("/me", s.CurrentUser, s.requireLogin)
 
-	page := group.Group("", middleware.CSRFWithConfig(middleware.CSRFConfig{
-		CookieName:  "_oauth_csrf",
-		CookiePath:  s.config.Mount,
-		TokenLookup: "form:csrf",
-	}))
+	page := root.Group("", csrfMiddleware)
 	page.GET("/login", webui.Template.Handler, s.redirectToNext)
 	page.GET("/logout", s.ClientLogout)
 	page.GET("/authorize", s.Authorize(webui.Template.Handler), s.redirectToLogin)
 	page.POST("/authorize", s.Authorize(webui.Template.Handler), s.redirectToLogin)
+	page.GET("/", webui.Template.Handler, s.redirectToLogin)
+	page.GET("/*", webui.Template.Handler)
 
-	group.GET("/", webui.Template.Handler, s.redirectToLogin)
-	group.GET("/*", webui.Template.Handler)
-
-	group.GET("/local-callback", s.redirectToLocal)
+	root.GET("/local-callback", s.redirectToLocal)
 
 	// No CSRF here:
-	group.POST("/token", s.Token)
+	root.POST("/token", s.Token)
 }

pkg/oauth/user.go
@@ -39,19 +39,14 @@ const authCookieName = "_session"
 func (s *server) authCookie() *cookie.Cookie {
 	return &cookie.Cookie{
 		Name:     authCookieName,
-		Path:     s.config.UI.MountPath(),
+		Path:     "/",
 		HTTPOnly: true,
 	}
 }
 
-type authCookie struct {
-	UserID    string `json:"user_id"`
-	SessionID string `json:"session_id"`
-}
-
 var errAuthCookie = errors.DefineUnauthenticated("auth_cookie", "could not get auth cookie")
 
-func (s *server) getAuthCookie(c echo.Context) (cookie authCookie, err error) {
+func (s *server) getAuthCookie(c echo.Context) (cookie auth.CookieShape, err error) {
 	ok, err := s.authCookie().Get(c, &cookie)
 	if err != nil {
 		return cookie, err
@@ -62,8 +57,8 @@ func (s *server) getAuthCookie(c echo.Context) (cookie authCookie, err error) {
 	return cookie, nil
 }
 
-func (s *server) updateAuthCookie(c echo.Context, update func(value *authCookie) error) error {
-	cookie := &authCookie{}
+func (s *server) updateAuthCookie(c echo.Context, update func(value *auth.CookieShape) error) error {
+	cookie := &auth.CookieShape{}
 	_, err := s.authCookie().Get(c, cookie)
 	if err != nil {
 		return err
@@ -119,7 +114,7 @@ func (s *server) getUser(c echo.Context) (*ttnpb.User, error) {
 	}
 	user, err := s.store.GetUser(
 		c.Request().Context(),
-		&ttnpb.UserIdentifiers{UserID: session.UserID},
+		&ttnpb.UserIdentifiers{UserID: session.UserIdentifiers.UserID},
 		nil,
 	)
 	if err != nil {
@@ -192,16 +187,26 @@ func (s *server) Login(c echo.Context) error {
 		return err
 	}
 	userIDs := ttnpb.UserIdentifiers{UserID: req.UserID}
+	tokenSecret, err := auth.GenerateKey(ctx)
+	if err != nil {
+		return err
+	}
+	hashedSecret, err := auth.Hash(ctx, tokenSecret)
+	if err != nil {
+		return err
+	}
 	session, err := s.store.CreateSession(ctx, &ttnpb.UserSession{
 		UserIdentifiers: userIDs,
+		SessionSecret:   hashedSecret,
 	})
 	if err != nil {
 		return err
 	}
 	events.Publish(evtUserLogin(ctx, userIDs, nil))
-	err = s.updateAuthCookie(c, func(cookie *authCookie) error {
-		cookie.UserID = session.UserID
+	err = s.updateAuthCookie(c, func(cookie *auth.CookieShape) error {
+		cookie.UserID = session.UserIdentifiers.UserID
 		cookie.SessionID = session.SessionID
+		cookie.SessionSecret = tokenSecret
 		return nil
 	})
 	if err != nil {