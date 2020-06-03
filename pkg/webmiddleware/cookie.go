nejraselimovic/lorawan-stack-1

util: Add cookie auth realted web middlewares

pkg/webmiddleware/cookie.go
@@ -1,0 +1,45 @@
+// Copyright © 2020 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package webmiddleware
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/gorilla/securecookie"
+)
+
+type ctxKeyType struct{}
+
+var ctxKey ctxKeyType
+
+// Cookies is a middleware that allows handling of secure cookies.
+func Cookies(block, hash []byte) MiddlewareFunc {
+	s := securecookie.New(hash, block)
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+
+			next.ServeHTTP(w, r.WithContext(context.WithValue(ctx, ctxKey, s)))
+		})
+	}
+}
+
+// GetEncoder retrieves the secure cookie encoder from the context.
+func GetEncoder(ctx context.Context) (encoder *securecookie.SecureCookie, err error) {
+	encoder = ctx.Value(ctxKey).(*securecookie.SecureCookie)
+
+	return encoder, nil
+}

vojta-novak/lorawan-stack

pkg/webmiddleware/cookie.go
@@ -1,0 +1,45 @@
+// Copyright © 2020 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package webmiddleware
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/gorilla/securecookie"
+)
+
+type ctxKeyType struct{}
+
+var ctxKey ctxKeyType
+
+// Cookies is a middleware that allows handling of secure cookies.
+func Cookies(hashKey, blockKey []byte) MiddlewareFunc {
+	s := securecookie.New(hashKey, blockKey)
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			ctx = context.WithValue(ctx, ctxKey, s)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
+
+// GetSecureCookie retrieves the secure cookie encoder from the context.
+func GetSecureCookie(ctx context.Context) (secureCookie *securecookie.SecureCookie, err error) {
+	secureCookie = ctx.Value(ctxKey).(*securecookie.SecureCookie)
+
+	return secureCookie, nil
+}