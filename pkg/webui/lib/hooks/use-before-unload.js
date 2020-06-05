a3d17e365b2ee561894909cfcb03cb634849ac0e
Bogdan <bogdans.afonins@thethingsindustries.com>
2020-06-05 13:27:43 +0300

    fixup! console,oauth: Fix use-before-unload hook
    add jsdoc

--- pkg/webui/lib/hooks/use-before-unload.js
+++ pkg/webui/lib/hooks/use-before-unload.js
@@ -14,6 +14,27 @@
 
 import React from 'react'
 
+/**
+ * React hook that allows subscribing to `window.onbeforeunload` and control it inside
+ * a react functional component.
+ *
+ * @param {Function} handler - The event handler function.
+ * @example
+ *
+ * // Just prompt the user if page unload is indeed needed.
+ * useBeforeUnload(event => {
+ *  if (shouldBlock) {
+ *    event.preventDefault()
+ *  }
+ * })
+ *
+ * // Use custom message when prompting the user. Note: this is not support by most browsers.
+ * useBeforeUnload(event => {
+ *  if (shouldBlock) {
+ *    return "Some text"
+ *  }
+ * })
+ */
 const useBeforeUnload = (handler = () => {}) => {
   const handlerRef = React.useRef(handler)
 
